926b95ef710423477c5accd54db57a15390e1bc7
AmySelsor <a_sels@me.com>
2012-06-20 02:17:36 -0500

    initial eichler web commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EichlerWeb::Application.config.secret_token = 'c6cb6d40671652757bf254011848c181d8e3bae04507297b0531fa6af5e7c93a9cf1f6d55dc768c5b8ab6270c3cfe0e9b7f76ea59b034adf10c08fdbee67a330'
